38e0e3817db0d7933fb75fd7123b63789e141ec4
russell <russell@104.39.80.195>
2018-04-24 13:57:51 -0400

    created loginModel

---
+++ src/LoginModel.java
@@ -0,0 +1,24 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author russell
+ */
+public class LoginModel {
+    private Controller cntl;
+    private HashMap<String, String> loginInfo;
+    LoginModel(Controller cntl)
+    {
+        this.cntl = cntl;
+        this.loginInfo.put("richChump", "baconBitz");
+    }
+    
+    
+}
