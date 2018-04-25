e06ebb5797b4bac27c2e762198c9c0467f419f66
derek <derek@104.39.85.102>
2018-04-25 15:06:17 -0400

    Andrea: Is trying her best to help add clickable images. Added two clickable buttons that are the LionCat and Dragon images!! <--YES

--- src/LoginModel.java
+++ src/LoginModel.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 public class LoginModel {
     private final Controller cntl;
     private HashMap<String, String> loginInfo;
+    
     LoginModel(Controller cntl)
     {
         this.cntl = cntl;
--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,12 +1,13 @@
+
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 
 /**
  *
@@ -21,19 +22,19 @@ import javax.swing.JPanel;
     In constructor
     mainGUI = new MainGUI(this);
     mainGUI.setVisible(true);
-*/
-public class MainGUI extends JFrame{
+ */
+public class MainGUI extends JFrame {
+
     private Controller controller; // controller
     private JPanel titlePanel;  // panel
+    private JPanel animalPanel; //Panel to hold AMINALS
     
-    public MainGUI(Controller controller)
-    {
+    public MainGUI(Controller controller) {
         this.controller = controller;
         mainGUIComponents();
     }
-    
-    public void mainGUIComponents()
-    {
+
+    public void mainGUIComponents() {
         /*
         Ari suggested making everything panels, that way you can format
         each panel the way you like, then add said pannel to the main frame.
@@ -49,34 +50,49 @@ public class MainGUI extends JFrame{
         that would add the title panel to the north part, you could have the cart
         to the east, and then add all the products we want to a panel and add
         that to the center.
-        */
-        
-        titlePanel = new JPanel(new GridLayout(1,2)); // rows, columns
+         */
+
+        titlePanel = new JPanel(new GridLayout(1, 2)); // rows, columns
         JButton cartButton = new JButton("Cart");
         cartButton.setToolTipText("Opens cart !"); // when the cart button is hovered over, "Opens cart !" messge pops up to user indicating what that button does
-        cartButton.addActionListener(event -> controller.cartButtonPressed()); 
-        
+        cartButton.addActionListener(event -> controller.cartButtonPressed());
+
         //HEADER OF MAIN GUI
         JLabel label = new JLabel("The Brant-Bargain Store"); // creates header for main view
         label.setFont(new Font("Courier", Font.BOLD, 25)); // sets the font of the header
-        
+
         //STORE IMAGE
         JLabel storeImage = new JLabel(new ImageIcon("B-BLogo.png"));//Store image, put image path in quotes
-        
+
         // ADDING TO PANELS
         titlePanel.add(storeImage);
         titlePanel.add(label);
         titlePanel.add(cartButton);
-        
-       
+
         // ADDING TO MAIN GUI
         this.setTitle("Home Screen"); // title of MainGUI     "Home Screen" is a placeholder   'this' refers to MainGUI JFrame
-        this.setSize(1100,800); //width,height of MainGUI frame,  size is placeholder
+        this.setSize(1100, 800); //width,height of MainGUI frame,  size is placeholder
         this.setLocationRelativeTo(null); // centers the panel on the screen
-        this.setContentPane(new JPanel (new BorderLayout())); // sets main pane to be a border layout
+        this.setContentPane(new JPanel(new BorderLayout())); // sets main pane to be a border layout
+
+        
+        animalPanel = new JPanel(new GridLayout(1, 2));
+        //Andrea's desperate attempt to keep adding value to this project :-(
+        //Adding Clickable images of the Animals
+        Icon lionCat = new ImageIcon(getClass().getResource("SuperSaiyanLionCat.png"));
+        JButton lionCatButton = new JButton(lionCat);
+        animalPanel.add(lionCatButton);
+        
+        Icon godzillaDragon = new ImageIcon(getClass().getResource("godzillaDragon.png"));
+        JButton dragonButton = new JButton(godzillaDragon);
+        animalPanel.add(dragonButton);
+        
+        this.getContentPane().add(animalPanel, BorderLayout.SOUTH);
+
         
         // All code below adds components to MainGUI JFrame
         this.getContentPane().add(titlePanel, BorderLayout.NORTH); // title panel added to top of JFrame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //when 'x' is clicked on window, program terminates
+        
     }
 }
