a55d688b14892f617773088f4d73d911205f666d
nickbradstreet <nickbradstreet@nicholass-mbp>
2018-04-25 16:30:24 -0400

    Added Russell

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -14,21 +14,11 @@ import javax.swing.JPanel;
  * @author Nick Bradstreet
  */
 
-/*
-    IN CONTROLLER CLASS:
-    Before constructor
-    MainGUI mainGUI;
-
-    In constructor
-    mainGUI = new MainGUI(this);
-    mainGUI.setVisible(true);
- */
 public class MainGUI extends JFrame {
 
     private Controller controller; // controller
     private JPanel titlePanel;  // panel
-    private JPanel animalPanelTop; //TopPanel to hold AMINALS
-    private JPanel animalPanelBottom;
+    private JPanel animalPanel;
 
     public MainGUI(Controller controller) {
         this.controller = controller;
@@ -36,73 +26,51 @@ public class MainGUI extends JFrame {
     }
 
     public void mainGUIComponents() {
-        /*
-        Ari suggested making everything panels, that way you can format
-        each panel the way you like, then add said pannel to the main frame.
         
-        For now I created a title panel (can hold title, company pic, etc)
-        A controle button panel for buttons to navigate the main view
-        And a cart button panel that will have the button to link to cart view
-        Each button panel can have one or many buttons, doing it like this gives
-        us the flexibility on how many buttons we want, and can then use
-        BorderLayout to put the panels in place.
+        //HEADER OF MAIN GUI
+        JLabel label = new JLabel("The Brant-Bargain Store"); // creates header for main view
+        label.setFont(new Font("Courier", Font.BOLD, 25)); // sets the font of the header
         
-        EX. this.getContentPane().add(titlePanel, BorderLayout.NORTH);
-        that would add the title panel to the north part, you could have the cart
-        to the east, and then add all the products we want to a panel and add
-        that to the center.
-         */
-
         titlePanel = new JPanel(new GridLayout(1, 2)); // rows, columns
         JButton cartButton = new JButton("Cart");
         cartButton.setToolTipText("Opens cart !"); // when the cart button is hovered over, "Opens cart !" messge pops up to user indicating what that button does
         cartButton.addActionListener(event -> controller.cartButtonPressed());
 
-        //HEADER OF MAIN GUI
-        JLabel label = new JLabel("The Brant-Bargain Store"); // creates header for main view
-        label.setFont(new Font("Courier", Font.BOLD, 25)); // sets the font of the header
-
-        //STORE IMAGE
-        JLabel storeImage = new JLabel(new ImageIcon("B-BLogo.png"));//Store image, put image path in quotes
 
         // ADDING TO PANELS
-        titlePanel.add(storeImage);
         titlePanel.add(label);
         titlePanel.add(cartButton);
+        animalPanel = new JPanel(new GridLayout(2, 3));
+        Icon lionCat = new ImageIcon(getClass().getResource("SuperSaiyanLionCat.png"));
+        JButton lionCatButton = new JButton(lionCat);
+        animalPanel.add(lionCatButton);
+
+        Icon godzillaDragon = new ImageIcon(getClass().getResource("godzillaDragon.png"));
+        JButton dragonButton = new JButton(godzillaDragon);
+        animalPanel.add(dragonButton);
+
+        Icon bird = new ImageIcon(getClass().getResource("ThisisdefinitelyabirdNOTabat.png"));
+        JButton birdButton = new JButton(bird);
+        animalPanel.add(birdButton);
+
+        Icon unicorns = new ImageIcon(getClass().getResource("unicornCats.png"));
+        JButton unicornButton = new JButton(unicorns);
+        animalPanel.add(unicornButton);
+        
+        Icon dog = new ImageIcon(getClass().getResource("NiaDogO.png"));
+        JButton dogButton = new JButton(dog);
+        animalPanel.add(dogButton);
+        
+        Icon russell = new ImageIcon(getClass().getResource("russell.png"));
+        JButton russellButton = new JButton(russell);
+        animalPanel.add(russellButton);
 
         // ADDING TO MAIN GUI
         this.setTitle("Home Screen"); // title of MainGUI     "Home Screen" is a placeholder   'this' refers to MainGUI JFrame
         this.setSize(1100, 800); //width,height of MainGUI frame,  size is placeholder
         this.setLocationRelativeTo(null); // centers the panel on the screen
         this.setContentPane(new JPanel(new BorderLayout())); // sets main pane to be a border layout
-
-        animalPanelTop = new JPanel(new GridLayout(1, 2));
-        animalPanelBottom = new JPanel(new GridLayout(1, 2));
-        //Andrea's desperate attempt to keep adding value to this project :-(
-        //Adding Clickable images of the Animals
-        Icon lionCat = new ImageIcon(getClass().getResource("SuperSaiyanLionCat.png"));
-        JButton lionCatButton = new JButton(lionCat);
-        animalPanelTop.add(lionCatButton);
-
-        Icon godzillaDragon = new ImageIcon(getClass().getResource("godzillaDragon.png"));
-        JButton dragonButton = new JButton(godzillaDragon);
-        animalPanelTop.add(dragonButton);
-
-        Icon bird = new ImageIcon(getClass().getResource("ThisisdefinitelyabirdNOTabat.png"));
-        JButton birdButton = new JButton(bird);
-        animalPanelTop.add(birdButton);
-
-        Icon unicorns = new ImageIcon(getClass().getResource("unicornCats.png"));
-        JButton unicornButton = new JButton(unicorns);
-        animalPanelBottom.add(unicornButton);
-        //This is Andrea's DogO. Yes you can compliment her.
-        Icon dog = new ImageIcon(getClass().getResource("NiaDogO.png"));
-        JButton dogButton = new JButton(dog);
-        animalPanelBottom.add(dogButton);
-
-        this.getContentPane().add(animalPanelTop, BorderLayout.CENTER);
-        this.getContentPane().add(animalPanelBottom, BorderLayout.SOUTH);
-        // All code below adds components to MainGUI JFrame
+        this.getContentPane().add(animalPanel, BorderLayout.CENTER);
         this.getContentPane().add(titlePanel, BorderLayout.NORTH); // title panel added to top of JFrame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //when 'x' is clicked on window, program terminates
 
