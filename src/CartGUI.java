72918d0e5e1a9cceb10f898e1f07427ffb8a6b3b
Patrick <Patrick@104.39.192.28>
2018-04-25 17:35:59 -0400

    Cart GUI update 4

--- src/CartGUI.java
+++ src/CartGUI.java
@@ -15,18 +15,22 @@ import java.awt.event.*;
 import javax.swing.JOptionPane;
 
 public class CartGUI extends JFrame{
-    private JTextField messageField;
-    private JButton checkoutMessageButton;
-    private JButton returnMessageButton;
-    private JPanel cartButtonPanel;
-    private JPanel cartInfoPanel;
-    private JPanel cartContentPanel;
-    private JPanel cartTotalPanel;
-    private JLabel label1;
-    private JLabel label2;
-    private JLabel totalLabel;
+    private JButton checkoutMessageButton;//page end panel
+    private JButton returnMessageButton;//page end panel
     
-    private JLabel example;
+    private JPanel cartButtonPanel;//page end/bottom panel
+    private JPanel cartInfoPanel;//page start/top panel
+    private JPanel cartContentPanel;//center panel
+    private JPanel cartTotalPanel;//page end/bottom panel
+    
+    private JLabel label1;//page start labeling item
+    private JLabel label2;//page start labeling price
+    
+    private JLabel totalLabel;//page end labeling total price
+    private JTextField totalPrice;//page end displaying total price
+    
+    private JLabel animalnamehere; //cartContentPanel
+    private JLabel animalpricehere; //cartContentPanel
     
    
     private Controller controller;
@@ -39,11 +43,21 @@ public class CartGUI extends JFrame{
     
     public void cartGUIComponents()
     {
+        //top panel/page start
         label1 = new JLabel("Item(s)", SwingConstants.CENTER);
         label2 = new JLabel("Price", SwingConstants.CENTER);
+        
+        //label for the bottom panel
         totalLabel = new JLabel("Total:", SwingConstants.CENTER);
         
-        example = new JLabel("placeholder", SwingConstants.CENTER);
+        //***************************************************
+        totalPrice = new JTextField("$" + " your data here");//area for sum of prices to be placed
+        //***************************************************
+        
+        totalPrice.setHorizontalAlignment(SwingConstants.CENTER);
+        totalPrice.setEditable(false);
+        animalnamehere = new JLabel("animal name placeholder", SwingConstants.CENTER);//testing use
+        animalpricehere = new JLabel("price placeholder", SwingConstants.CENTER);//testing use
         
         returnMessageButton = new JButton("Return to Shopping");
         //return shopping button^
@@ -52,31 +66,43 @@ public class CartGUI extends JFrame{
         //want to make this when pressed pop up a JOptionpane to say:
         //"Thanks for the monies! Your new animal will NEVER arrive! :D"
         
-        cartButtonPanel = new JPanel(new GridLayout(1,2));
+        cartButtonPanel = new JPanel(new GridLayout(2,2));
         cartInfoPanel = new JPanel(new GridLayout(1,2));
         cartContentPanel = new JPanel(new GridLayout(0,2));
         cartTotalPanel = new JPanel(new GridLayout(1,2));
         
+        
+        //cart info panel - top of page. labels contents (Item, price)
         cartInfoPanel.add(label1);
         cartInfoPanel.add(label2);
         
-        cartContentPanel.add(example);
         
-        cartTotalPanel.add(totalLabel);
+        //cart content panel - should basically be centered between
+        //top panel(info) and bottom panel(buttons)
+        cartContentPanel.add(animalnamehere);
+        cartContentPanel.add(animalpricehere);
         
+        
+        //cart total panel - should be at the bottom but right above buttons
+        //cartTotalPanel.add(totalLabel);
+        
+        
+        //cart button panel - bottom most panel
+        /*test*/cartButtonPanel.add(totalLabel);
+        /*test*/cartButtonPanel.add(totalPrice);
         cartButtonPanel.add(returnMessageButton);
         cartButtonPanel.add(checkoutMessageButton);
-        
+                
         
         
         this.setTitle("Your shopping cart");
         this.setSize(500, 500);
         this.setLocationRelativeTo(null);
         this.setContentPane(new JPanel(new BorderLayout()));
-        this.getContentPane().add(cartInfoPanel, BorderLayout.NORTH);
+        this.getContentPane().add(cartInfoPanel, BorderLayout.PAGE_START);
         this.getContentPane().add(cartContentPanel, BorderLayout.CENTER);
-        this.getContentPane().add(cartTotalPanel, BorderLayout.CENTER);
-        this.getContentPane().add(cartButtonPanel, BorderLayout.SOUTH);
+        //this.getContentPane().add(cartTotalPanel, BorderLayout.CENTER);
+        this.getContentPane().add(cartButtonPanel, BorderLayout.PAGE_END);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }    
 }
