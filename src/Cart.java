dbb0d2cbcb936733f827d55dd481fc24e3ea9b69
russell <russell@104.39.80.195>
2018-04-26 13:30:58 -0400

    table model

--- src/Cart.java
+++ src/Cart.java
@@ -9,8 +9,10 @@
  * @author russell
  */
 import java.util.*;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 
-public class Cart {
+public class Cart implements TableModel {
     private LinkedList<Animal> cart;
     private Controller cntl;
     
@@ -34,5 +36,66 @@ public class Cart {
     {
         // or cart.stream().map(Animal::getCost()).sum(); <-- this might work too still working on it
         return cart.stream().map((a) -> a.getCost()).reduce(0.0, (a, b) -> a + b);
+    }
+
+    @Override
+    public int getRowCount() {
+        return 2;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return cart.size();
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        switch(columnIndex)
+        {
+            case 0:
+                return "Name";
+            case 1:
+                return "Price";
+        }
+        
+        return "Unused Column";
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch(columnIndex)
+        {
+            case 0:
+                return (Object) cart.get(rowIndex).getName();
+            case 1:
+                return (Object) ("$" + cart.get(rowIndex).getCost());
+        }
+        
+        return "unused";
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
